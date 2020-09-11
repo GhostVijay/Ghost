ec2446258705e999e781a5bef28e8ad6981b753f
GhostVijay <60440812+GhostVijay@users.noreply.github.com>
2020-09-11 15:57:14 +0530

    Update time.php

--- time.php
+++ time.php
@@ -1,11 +1,1 @@
-<?php
-$timezone =0;
-$now = date("Y-m-d", time()+$timezone);
-$time = date("H:i:s", time()+$timezone);
-list($year, $month, $day) = explode('-', $now);
-list($hour, $minute, $second) = explode(':', $time);
-$timestamp = mktime($hour, $minute, $second, $month, $day, $year);
-include ('jdf.php');
-$mrnimaw = jdate("time: H:i:s - date: Y/m/d",$timestamp);
-echo $mrnimaw;
-?>
+GHOST
